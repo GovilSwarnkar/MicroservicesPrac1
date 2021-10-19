65c2e15a58cb5de108f4264b60ed6b4c32d04adf
GovilSwarnkar <aryagovilswarnnkar@gmail.com>
2021-10-19 15:39:10 +0530

    rest template to measurement conversion from measurement calculation

--- MeasurementCalculation/src/main/java/com/microservices/prac/controller/MeasurementCalculationController.java
+++ MeasurementCalculation/src/main/java/com/microservices/prac/controller/MeasurementCalculationController.java
@@ -1,10 +1,14 @@
 package com.microservices.prac.controller;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.microservices.prac.bean.MeasurementConversion;
 
@@ -13,6 +17,15 @@ public class MeasurementCalculationController {
 
 	@GetMapping("/measurement-calculation/from/{from}/to/{to}/factor/{factor}")
 	public MeasurementConversion getMeasurementCalculatedValue(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal factor) {
-		return new MeasurementConversion(1001, from, to, factor,  BigDecimal.valueOf(10), factor.multiply(BigDecimal.valueOf(23)), 0);
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		ResponseEntity<MeasurementConversion> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/measurement-conversion/from/{from}/to/{to}",  MeasurementConversion.class, uriVariables);
+		
+		MeasurementConversion measurementConversion = responseEntity.getBody();
+		return new MeasurementConversion(measurementConversion.getId(), from, to, factor,  measurementConversion.getConversionMultiple(), 
+				factor.multiply(measurementConversion.getConversionMultiple()), measurementConversion.getPort());
 	}
 }

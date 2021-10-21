cc248ffc227c46d4d11b384ef9a9b18c68dcd6c3
GovilSwarnkar <aryagovilswarnnkar@gmail.com>
2021-10-21 12:08:01 +0530

    eureka naming server

---
+++ NetflixEurekaNamingServer/src/test/java/com/microservices/prac/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
